e5a6d5e1a7783034a83483524c19ec471ade1eeb
Jason Wicks <jasonkwicks@gmail.com>
2020-08-09 17:04:29 +1000

    Add bookshelf terraform code

---
+++ go/bookshelf/terraform/project/outputs.tf
@@ -0,0 +1,9 @@
+output "project_info_example" {
+  value       = module.project-factory.project_id
+  description = "The ID of the created project"
+}
+
+output "domain_example" {
+  value       = module.project-factory.domain
+  description = "The organization's domain"
+}
---
+++ go/bookshelf/terraform/project/vars.tfvars
@@ -0,0 +1,2 @@
+billing_account="01924A-67C8AB-47BFFE"
+organization_id=160486659853
